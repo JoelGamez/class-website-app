dac45625ffa631a0d35aeead2b8b1aec8fecc52e
joel gamez <joelgamez14six@yahoo.com>
2018-08-14 16:37:52 -0400

    admin controllers altered

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_scope :admin do
+    authenticated :admin do
+      root                to: "posts#index",
+                          as: :authenticated_user_root
+    end
+  end
+
+  devise_for :admins,      path: "admins",
+  controllers: {
+    sessions:           "admins/sessions",
+    registrations:      "admins/registrations",
+    passwords:          "admins/passwords"
+  }
+
   resources :posts
   root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
