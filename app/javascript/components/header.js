65002badd8834ff1a4f851f8c2f829c6ed86a9a6
joel gamez <joelgamez14six@yahoo.com>
2018-08-15 16:22:35 -0400

    Revert "react-bootstrap navbar added"
    This reverts commit ffc95a1d4f3c6a160d69ac374a070b6316a160f2.

--- app/javascript/components/header.js
+++ app/javascript/components/header.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import PostContainer from './postContainer'
 import {Route, Switch, Link, NavLink} from 'react-router-dom'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 
 
@@ -12,42 +11,12 @@ class Header extends React.Component {
   render(){
     return (
       <div>
-        <Navbar inverse style={{backgroundColor:'#00ffa5',borderColor:'#ffffff'}}>
-          <Navbar.Header  pullLeft >
-            <Navbar.Brand >
-              <a  id='logo' pullLeft >HQ3</a>
-            </Navbar.Brand>
-            <Navbar.Toggle style={{borderColor:'#ffffff'}} />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav  pullLeft>
-              <NavItem eventKey={1} href="#">
-                    <NavLink exact activeStyle={{color:"purple"}}to='/'>Home</NavLink>
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                  <NavLink activeStyle={{color:"purple"}}to='/about'>About</NavLink>
-              </NavItem>
-              <NavItem eventKey={3} href="#">
-                  <NavLink activeStyle={{color:"purple"}}to='/tutorials'>Tutorials</NavLink>
-              </NavItem>
-              <NavItem eventKey={4} href="#">
-                  <NavLink activeStyle={{color:"purple"}}to='/posts'>Posts</NavLink>
-              </NavItem>
-              <NavItem eventKey={5} >
-                  <a href='/admins/sign_in'>signin</a>
-              </NavItem>
+        <NavLink exact activeStyle={{color:"purple"}}to='/'>Home</NavLink>
+        <NavLink activeStyle={{color:"purple"}}to='/about'>About</NavLink>
+        <NavLink activeStyle={{color:"purple"}}to='/tutorials'>Tutorials</NavLink>
+        <NavLink activeStyle={{color:"purple"}}to='/posts'>Posts</NavLink>
+        <a href='/admins/sign_in'>signin</a>
 
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">
-                Fun Games
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Local Events
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
       </div>
     )
   }
