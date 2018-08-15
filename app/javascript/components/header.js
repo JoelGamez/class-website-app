128e4ecd0f4cf5b2afc20bde01c085afb4f063cf
joel gamez <joelgamez14six@yahoo.com>
2018-08-15 16:59:55 -0400

    navBar has css

--- app/javascript/components/header.js
+++ app/javascript/components/header.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import PostContainer from './postContainer'
 import {Route, Switch, Link, NavLink} from 'react-router-dom'
+import {Navbar, Nav, NavItem} from 'react-bootstrap/lib'
 
 
 
@@ -11,12 +12,41 @@ class Header extends React.Component {
   render(){
     return (
       <div>
-        <NavLink exact activeStyle={{color:"purple"}}to='/'>Home</NavLink>
-        <NavLink activeStyle={{color:"purple"}}to='/about'>About</NavLink>
-        <NavLink activeStyle={{color:"purple"}}to='/tutorials'>Tutorials</NavLink>
-        <NavLink activeStyle={{color:"purple"}}to='/posts'>Posts</NavLink>
-        <a href='/admins/sign_in'>signin</a>
-
+          <Navbar className='pageNav' inverse collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a id='logo'>HQ3</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">
+                  <NavLink exact activeStyle={{color:"#fbe547"}}to='/'>Home</NavLink>
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                  <NavLink activeStyle={{color:"#fbe547"}}to='/about'>About</NavLink>
+                </NavItem>
+                <NavItem eventKey={3} href="#">
+                  <NavLink activeStyle={{color:"#fbe547"}}to='/tutorials'>Tutorials</NavLink>
+                </NavItem>
+                <NavItem eventKey={4} href="#">
+                  <NavLink activeStyle={{color:"#fbe547"}}to='/posts'>Posts</NavLink>
+                </NavItem>
+                <NavItem eventKey={5} >
+                  <a href='/admins/sign_in'>signin</a>
+                </NavItem>
+              </Nav>
+              <Nav pullRight>
+                <NavItem eventKey={1} href="#">
+                    <a >Resources</a>
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    <a >Local Events</a>
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
       </div>
     )
   }
