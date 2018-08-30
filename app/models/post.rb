1980ca138e74af0c003710dfe157b477ab0ae80c
joel gamez <joelgamez14six@yahoo.com>
2018-08-29 23:05:11 -0400

    methods added to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,5 +12,39 @@ class Post < ApplicationRecord
     return recent_post
   end
 
+  def topic_lists
+   topics = []
+
+   topics.push("HW") if  self.is_hw
+   topics.push("Math")if self.is_math
+   topics.push("Spanish") if self.is_spanish
+   topics.push("Science")if self.is_science
+   topics.push("Tutorial") if self.is_tutorial
+   topics.push("Class-Project") if self.is_class_project
+   topics.push("Event") if self.is_event
+
+
+
+
+
+  return topics
+
+
+ end
+
+
+  def string_topic_lists(array)
+    topics = ""
+    for i in array
+      if(i == array[-1])
+        topics += "#{i}"
+      else
+         topics += "#{i} | "
+      end
+
+    end
+
+    return topics
+  end
 
 end
