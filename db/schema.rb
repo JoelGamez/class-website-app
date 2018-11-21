a4d1f95dce471454b30ca1536058c2c99a633f7b
joel gamez <joelgamez14six@yahoo.com>
2018-11-21 12:26:48 -0500

    PG gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2018_10_09_223423) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -55,7 +58,7 @@ ActiveRecord::Schema.define(version: 2018_10_09_223423) do
     t.string "title"
     t.text "description"
     t.boolean "add_to_slide_show"
-    t.integer "admin_id"
+    t.bigint "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["admin_id"], name: "index_galleries_on_admin_id"
@@ -74,9 +77,11 @@ ActiveRecord::Schema.define(version: 2018_10_09_223423) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "admin_id"
+    t.bigint "admin_id"
     t.boolean "is_resource"
     t.index ["admin_id"], name: "index_posts_on_admin_id"
   end
 
+  add_foreign_key "galleries", "admins"
+  add_foreign_key "posts", "admins"
 end
